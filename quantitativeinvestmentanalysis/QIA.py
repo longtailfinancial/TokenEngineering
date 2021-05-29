ea2829608c4089ffa8d7ae5aeb19eafbe21118cd
Shawn Anderson <shawnltf@pm.me>
2021-05-29 13:06:38 -0700

    lowercase QIA/ package to conform to python standards.

--- quantitativeinvestmentanalysis/QIA.py
+++ quantitativeinvestmentanalysis/QIA.py
@@ -1,3 +1,9 @@
+"""
+This module is an implementation of the book Quantitative Investment Analysis, DeFusco, McLeavey, Pinto, Runkle, Second Edition, CFA Institute Investment Series, Wiley Publishing.
+You can find the 3rd edition of this book available here: https://www.wiley.com/en-gb/Quantitative+Investment+Analysis%2C+3rd+Edition-p-9781119104599
+"""
+
+
 import panel as pn
 import pandas as pd
 import numpy as np
