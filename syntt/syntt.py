b6cb386f57fe1ceb1d56252092616bc9fb8b605e
Johann Suarez <johann.suarez92@gmail.com>
2021-07-21 17:03:44 -0700

    A proposed decorator that will speed our workflow tremendously

--- syntt/syntt.py
+++ syntt/syntt.py
@@ -7,3 +7,16 @@ def clamp(n, smallest: int=0, largest: int=1000) -> float:
     '''
 
     return max(smallest, min(n, largest))
+
+
+def togglize(toggle_name):
+    '''
+    A Skeleton for what is planned
+    decorator to embed a panel widget
+    into a toggle control.
+
+    So the widget can be hidden/unhidden
+    with a button, with the button name being the provided
+    argument to the decorator.
+    '''
+    pass
