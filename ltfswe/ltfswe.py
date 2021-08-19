d48f254c2074b85ea48ee1cd1df9cdfde1d1b136
JohannSuarez <johann.suarez92@gmail.com>
2021-08-19 15:43:17 -0700

    Added Generic Mediator class.

--- ltfswe/ltfswe.py
+++ ltfswe/ltfswe.py
@@ -4,7 +4,43 @@ from functools import wraps
 pn.extension()
 
 
-def clamp(n, smallest: int=0, largest: int=1000) -> float:
+class BaseMediator():
+    '''
+    Generic Mediator
+    '''
+
+    def __init__(self: object, *argv) -> None:
+        """
+        The generic mediator must take on a variable number of 'colleagues'
+        """
+        self.colleague_list = list(argv)
+
+    def __repr__(self: object) -> None:
+        '''
+        We can specify how this object will be represented in
+        interactive/realtime environments and make it display
+        useful information.
+
+        This __repr__() method will be invoked when you run repr(Mediator)
+        or just print out the name of an instantiation of it.
+        '''
+
+        msg = 'Mediator object.\nCurrently watching:\n'
+        for colleague in self.colleague_list:
+            msg = msg + \
+                f'Colleague: {colleague}\nType: type{type(colleague)}\n'
+
+        return msg
+
+    @staticmethod
+    def notify(sender, event) -> None:
+        pass
+
+    def read_models(self: object) -> None:
+        pass
+
+
+def clamp(n, smallest: int = 0, largest: int = 1000) -> float:
     '''
     Binding an int or float
     to a minimum and maximum value.
@@ -13,12 +49,11 @@ def clamp(n, smallest: int=0, largest: int=1000) -> float:
     return max(smallest, min(n, largest))
 
 
-
-def togglize(toggle_name: str="Hide / Unhide Widget", color: str="green"):
+def togglize(toggle_name: str = "Hide / Unhide Widget", color: str = "green"):
 
     def decorator_function(original_function):
 
-        @wraps(original_function) # So we can stack decorators. 
+        @wraps(original_function)  # So we can stack decorators.
         def wrapper_function(*args, **kwargs):
             '''
             In this block are the boilerplate
@@ -26,13 +61,14 @@ def togglize(toggle_name: str="Hide / Unhide Widget", color: str="green"):
             '''
 
             tog_color = {
-              "green": "success",
-              "red": "danger",
-              "blue": "primary"
+                "green": "success",
+                "red": "danger",
+                "blue": "primary"
             }
 
+            toggle = pn.widgets.Toggle(
+                name=toggle_name, button_type=tog_color[color])
 
-            toggle = pn.widgets.Toggle(name=toggle_name, button_type=tog_color[color])
             @pn.depends(toggle)
             def toggle_watch(x):
                 if x:
@@ -51,8 +87,6 @@ def togglize(toggle_name: str="Hide / Unhide Widget", color: str="green"):
             sample_widget_plot = pn.Column(pn.Column(toggle), toggle_watch)
             return sample_widget_plot
 
-
             return result
         return wrapper_function
     return decorator_function
-
--- ltfte/ltfte.py
+++ ltfte/ltfte.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import hvplot.pandas
 import holoviews as hv
-import param as pm 
+import param as pm
 import random
 import math
 import warnings
@@ -20,7 +20,7 @@ class Sigmoid(pm.Parameterized):
 
     Attributes
     ----------
-    l : number that adjusts the y-axis scale 
+    l : number that adjusts the y-axis scale
         default=20.8, bounds=(0, 100)
     s : number that adjusts the supply of tokens
         default=17, bounds=(1, 20)
@@ -28,7 +28,7 @@ class Sigmoid(pm.Parameterized):
         default=21e6, bounds=(1, 21e6)
     k : number of tokens sold
         default=57300, bounds=(1, 1e5)
-    steps : Integer 
+    steps : Integer
         default=1000, bounds=(10, 10000)
     zoom : number that affects the scale of the presented view
         default=0.03, bounds=(0.01, 1)
@@ -42,13 +42,13 @@ class Sigmoid(pm.Parameterized):
 
     x():
         returns values for x axis scale based on m, zoom, steps.
-   
+
     curve(x):
         Returns a dataframe containing the supply and price based on the X values
         supplied to f(x)
 
     collateral(x):
-        Creates and returns the dataframe containing values that are less than the 
+        Creates and returns the dataframe containing values that are less than the
         current token supply
 
     view_curve():
@@ -56,7 +56,7 @@ class Sigmoid(pm.Parameterized):
         and the price
 
     view_collateral():
-        Returns a holoviews area plot that shows the relationship between the 
+        Returns a holoviews area plot that shows the relationship between the
         supply and price
 
     view():
@@ -72,11 +72,11 @@ class Sigmoid(pm.Parameterized):
     steps = pm.Integer(1000, bounds=(10,10000), step=10, precedence=-1)
     zoom = pm.Number(0.03, bounds=(0.01,1),step=0.01)
     current_supply = pm.Number(10000, step=1000)
-    
+
     def __init__(self, **params):
         super(Sigmoid, self).__init__(**params)
         self.param['current_supply'].bounds = (1,self.m*self.zoom)
-    
+
     def f(self, x):
         """Parameterized Sigmoid Function"""
         self.param['current_supply'].bounds = (1,self.m*self.zoom)
@@ -85,23 +85,23 @@ class Sigmoid(pm.Parameterized):
     def x(self):
         x = np.linspace(0,self.m*self.zoom, self.steps)
         return x
-    
+
     def curve(self, x):
         y = self.f(x)
         return pd.DataFrame(zip(x,y),columns=['supply','price'])
-    
+
     def collateral(self, x):
         df = self.curve(x)
         return df[df['supply'] < self.current_supply]
-    
+
     def view_curve(self):
         x = self.x()
         return self.curve(x).hvplot.line(title='Bonding Curve', x='supply',y='price')
-    
+
     def view_collateral(self):
         x = self.x()
         return self.collateral(x).hvplot.area(x='supply',y='price')
-    
+
     def view(self):
         return self.view_curve()*self.view_collateral()
 
@@ -109,13 +109,13 @@ class Sigmoid(pm.Parameterized):
 ### Multisigmoid
 class MultiSigmoid(Sigmoid):
     """
-    A parameterized class to represent a Multi-Sigmoid curve that inherits 
+    A parameterized class to represent a Multi-Sigmoid curve that inherits
     all the methods and attributes from Sigmoid
 
     Attributes
     ----------
 
-    l : number that adjusts the y-axis scale 
+    l : number that adjusts the y-axis scale
         default=20.8, bounds=(0, 100)
 
     s : number that adjusts the supply of tokens
@@ -127,7 +127,7 @@ class MultiSigmoid(Sigmoid):
     k : number of tokens sold
         default=57300, bounds=(1, 1e5)
 
-    NOTE: Each of the additional Sigmoid have their own respective variables appropriately 
+    NOTE: Each of the additional Sigmoid have their own respective variables appropriately
           labeled with that curves number. (Ex: l2, s2, m2, k2)
 
     Methods
@@ -151,22 +151,22 @@ class MultiSigmoid(Sigmoid):
     s3 = pm.Number(5, bounds=(1,20), precedence=-1)
     m3 = pm.Number(5e5, bounds=(1, 21e6), step=50000, precedence=-1)
     k3 = pm.Number(50, bounds=(1, 21e6), step=100, precedence=-1)
-    
+
     l4 = pm.Number(6, bounds=(0, 100), precedence=-1)
     s4 = pm.Number(9, bounds=(1,20), precedence=-1)
     m4 = pm.Number(5e6, bounds=(1, 21e6), step=50000, precedence=-1)
     k4 = pm.Number(2e3, bounds=(1, 21e6), step=100, precedence=-1)
-    
+
     def __init__(self, **params):
         super(MultiSigmoid, self).__init__(**params)
         self.param['current_supply'].bounds = (1,self.m*self.zoom)
-    
+
     def f2(self, x):
         return self.k2/(1+np.exp(-x*self.l2/self.m2+self.s2))
-    
+
     def f3(self, x):
         return self.k3/(1+np.exp(-x*self.l3/self.m3+self.s3))
-    
+
     def f4(self, x):
         return self.k4/(1+np.exp(-x*self.l4/self.m4+self.s4))
 
@@ -181,7 +181,7 @@ class MultiSigmoid(Sigmoid):
 ### Augumented
 class Augmented(MultiSigmoid):
     """
-    A parameterized class to model the Augmented MultiSigmoid bonding Curve. 
+    A parameterized class to model the Augmented MultiSigmoid bonding Curve.
 
     Attributes
     ----------
@@ -199,7 +199,7 @@ class Augmented(MultiSigmoid):
         the supply, price, minted  tokens, reserve, and funding of the curve as a whole
 
     reserves():
-        Returns data frame of the collateral and includes the net gains of the curve 
+        Returns data frame of the collateral and includes the net gains of the curve
 
     view_collateral():
         Returns a holoviews plot of the collateral against the curve
@@ -209,7 +209,7 @@ class Augmented(MultiSigmoid):
 
     """
     reserve_rate = pm.Number(0.2, bounds=(0, 1), step=0.01)
-    
+
     def curve(self, x):
         y = self.f(x)
         curve = pd.DataFrame(zip(x,y),columns=['supply','price'])
@@ -218,17 +218,17 @@ class Augmented(MultiSigmoid):
         curve['reserve'] = curve['sell_price']*curve['minted']
         curve['funding'] = curve['price']*(1-self.reserve_rate)*curve['minted']
         return curve.bfill()
-    
+
     def reserves(self):
         x = self.x()
         reserves = self.collateral(x)
         reserves['net'] = reserves['funding'] + reserves['reserve']
         return reserves[['funding','reserve', 'net']].sum()
-    
+
     def view_collateral(self):
         x = self.x()
         return self.collateral(x).rename(columns={'price':'funding_price','sell_price':'reserve_price'}).hvplot.area(x='supply', y=['funding_price','reserve_price'], stacked=False, alpha=1)
-    
+
     def view_reserves(self):
         r = self.reserves().to_frame()
         r.columns = ['CAD']
@@ -239,7 +239,7 @@ class Augmented(MultiSigmoid):
 ### Smart
 class Smart(Augmented):
     """
-    A parameterized class to model the Smart augmented MultiSigmoid bonding Curve. 
+    A parameterized class to model the Smart augmented MultiSigmoid bonding Curve.
 
     Attributes
     ----------
@@ -252,21 +252,21 @@ class Smart(Augmented):
     -------
 
     collateral(x):
-        Creates and returns a data frame of the collateral against the curve and the 
+        Creates and returns a data frame of the collateral against the curve and the
         selling curve.
 
     curve(x):
-       Same as the curve function from earlier, but missing values are now filled with 
-       .bfill() from pandas 
+       Same as the curve function from earlier, but missing values are now filled with
+       .bfill() from pandas
 
     """
     reserve_power = pm.Integer(4, bounds=(0,4))
-    
+
     def __init__(self, **params):
         super(Smart, self).__init__(**params)
         self.reserve_rate = 1
         self.param['reserve_rate'].precedence = -1
-        
+
     def collateral(self, x):
         curve = self.curve(x)
         curve = curve[curve['supply'] < self.current_supply]
@@ -276,7 +276,7 @@ class Smart(Augmented):
         curve['reserve'] = curve['sell_price']*curve['minted']
         curve['funding'] = curve['price']*(1-reserve_rate)*curve['minted']
         return curve
-        
+
     def curve(self, x):
         y = self.f(x)
         curve = pd.DataFrame(zip(x,y),columns=['supply','price'])
@@ -293,49 +293,49 @@ class TokenEngineering(pm.Parameterized):
     number_of_initial_contracts = pm.Integer(2, bounds=(1, 10), step=1)
     new_contracts_per_month = pm.Number(0.33, bounds=(0, 3), step=0.33)
     office_expense = pm.Integer(7000, bounds=(3000,7000), step=50)
-    
+
     def salary_costs(self):
         cummulative = [i*self.number_employees*self.monthly_salary for i in range(self.number_months)]
         return cummulative
-    
+
     def office_expenses(self):
         cummulative = [i*self.office_expense for i in range(self.number_months)]
         return cummulative
-    
+
     def costs(self):
         return [a+b for a,b in zip(self.salary_costs(), self.office_expenses())]
-    
+
     def number_of_contracts(self):
         cummulative = [i*self.new_contracts_per_month + self.number_of_initial_contracts for i in range(self.number_months)]
         return cummulative
-    
+
     def contract_revenue(self):
         number_of_contracts = self.number_of_contracts()
         cummulative = [i*self.monthly_contract_size*number_of_contracts[i] for i in range(self.number_months)]
         return cummulative
-    
+
     def ltf_treasury(self):
         return [a-b for a,b in zip(self.contract_revenue(),self.costs())]
-    
+
     def cummulative_data(self):
         data = pd.DataFrame({
-            'Contract Revenue': self.contract_revenue(), 
+            'Contract Revenue': self.contract_revenue(),
             'Number of Contracts': self.number_of_contracts(),
-            'Salary Costs': self.salary_costs(), 
+            'Salary Costs': self.salary_costs(),
             'Net Profit': self.ltf_treasury(),
             'Office Expenses': self.office_expenses()})
         data.index.name = 'Month'
         return data
-    
+
     def results(self):
         return self.cummulative_data().iloc[[-1]]
-    
+
     def results_view(self):
         return self.results().reset_index().hvplot.table(title="Results")
-    
+
     def chart_view(self):
         return self.cummulative_data().hvplot.line(title='Cumulative Revenue, Costs, and Profit') * hv.HLine(0).opts(color='black', line_width=1.2)
-    
+
     def view_te(self):
         return lambda te: pn.Row(te, pn.Column(te.chart_view, te.results_view))
 
@@ -343,16 +343,16 @@ class TokenEngineering(pm.Parameterized):
 ### Bonding Curve
 
 class Bonding(Smart):
-    
+
     def batch_minted(self):
         return self.current_supply - self.collateral(self.x()).iloc[-1]['supply']
-    
+
     def batch_available(self):
         return self.collateral(self.x()).iloc[-1]['minted'] - self.batch_minted()
-    
+
     def current_price(self):
         return self.collateral(self.x()).iloc[-1]['price']
-    
+
     def mint(self, CAD: float, tol=1e-6):
         self.zoom = 0.05
         current_price = self.current_price()
@@ -362,19 +362,19 @@ class Bonding(Smart):
             self.current_supply += tol
             return self.mint(CAD)
         print("CAD:", CAD, 'Price:', current_price, 'Request:', requested, 'Batch Available:', batch_available)
-        
+
         if requested <= batch_available:
             self.current_supply += requested
             received = requested
             return received, current_price
-        
+
         else:
             self.current_supply += batch_available
             next_received, next_price =  self.mint(current_price*(requested-batch_available))
             total_received = next_received + batch_available
             weighted_price = current_price*(batch_available/total_received) + next_price*(next_received/total_received)
             return total_received, weighted_price
-        
+
 
     def view_market(self):
         df = pd.DataFrame({
@@ -385,7 +385,7 @@ class Bonding(Smart):
         df['price'] = df['price'].apply(lambda x: "${:,.2f}".format(x))
         df['marketcap'] = df['marketcap'].apply(lambda x: "${:,.0f}".format(x))
         return df.T
-    
+
     def view_abc(self):
         return lambda abc: pn.Row(abc,pn.Column(abc.view, pn.Row(abc.view_reserves, abc.view_market)))
 
@@ -399,11 +399,11 @@ class Corporate(Bonding):
     Org's Sustainability Loop
     '''
     debt = pm.Number()
-    
+
     def __init__(self, **params):
         super(Corporate, self).__init__(**params)
         self.update_debt_bounds()
-        
+
     def collateral(self, x):
         collateral = super(Corporate, self).collateral(x)
         collateral['price'] = np.where(collateral['funding'].cumsum() < self.debt, 0, collateral['price'])
@@ -418,7 +418,7 @@ class Corporate(Bonding):
         reserves['debt'] = self.debt
         reserves['net'] = reserves['net'] - self.debt
         return reserves[['funding','debt','reserve','net']]
-    
+
     @pm.depends('current_supply', watch=True)
     def update_debt_bounds(self):
         self.param['debt'].bounds = (0,self.reserves()['funding'])
